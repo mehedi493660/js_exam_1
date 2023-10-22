743c64ce3d418ef35ba052dea6323b447de8a1d4
mehedi493660 <144321420+mehedi493660@users.noreply.github.com>
2023-10-22 13:37:14 +0600

    Add files via upload

---
+++ exam js/exam.js
@@ -0,0 +1,218 @@
+// ans no-1...................................
+
+// var a = 100;
+// var b = 90;
+// var ans = a+b;
+
+// console.log(ans);
+
+// ans no-3.....................................
+// data type name-
+
+// String - "mehedi"
+// intiger - 1,2,3
+// float - 53.5 , 2.4
+
+// ans no-4.....................................
+
+//  var ar namkoroner jonno j sob bisoy khal rakhte hobe ta holo-
+// * surute number use kora jabe na
+//  * var er majhe kono space babohar kora jabe na.
+//  * js er keyword use kora jabe na.
+//  * var ar name alada alada hote hobe.
+//  * protiti var name uniqe hote.
+
+// ans no-5.....................................
+// var name = "Md Mehedi Hasan Rabby";
+// console.log(name)
+
+// ans no-6.....................................
+
+// arithmetic operator er kaj holo (+,-,*,/) etc ganitik kaj kora
+
+// ans no-7...............................................................
+
+
+// (** ar maddhome power er man bosano jai)
+// ( % er maddhome vagses paua jai)
+// (++ er maddhome var er man 1 kore briddi pai)
+// (-- er maddhome var er man 1 kore kome  jai)
+
+// ans no-8...............................................................
+
+// operator jader niea kaj kore tader operend bola hoi 1+2 ekhane 1 and 2 operend.
+
+// ans no-9...............................................................
+
+// var x = 10;
+// var y = 36;
+// var z = x+y;
+
+// console.log(z);
+
+
+// ans no-10...............................................................
+
+// var a = 375;
+// var b = 8;
+// var ans = a%b;
+// console.log(ans);
+
+// ans no-11...............................................................
+
+// var a = 8;
+// var b = 3;
+// var ans = a**b;
+// console.log(ans);
+
+// ans no-12...............................................................
+
+// var sub1 = 55;
+// var sub2 = 78;
+// var sub3 = 87;
+// var sub4 = 90;
+// var number_of_sub = 4;
+
+// var total_num = sub1+sub2+sub3+sub4;
+// var avg = total_num/number_of_sub;
+// console.log(avg);
+
+// ans no-15...............................................................
+
+// var x = 30;
+// var y = '23';
+// var y = parseInt(y)
+
+// var addition = x+y;
+// console.log(addition);
+
+// ans no-15...............................................................
+
+// var a = '25';
+// var a = parseInt(a)
+// console.log(a);
+
+
+// ans no-16...............................................................
+
+// arrai index 0 theke suru hoi
+
+
+// ans no-17...............................................................
+
+// var numbers = [34,56,74,24,88,34,776,34,5]
+// numbers[5]=334;
+// console.log(numbers);
+
+// ans no-18...............................................................
+
+// var numbers = [34,56,74,24,88,34,776,34,5]
+// numbers.push(234);
+// numbers.unshift(998);
+// console.log(numbers);
+
+// ans no-19...............................................................
+
+// var numbers = [[1,2],[3,4],[5,6]]
+// console.log(numbers.flat());
+
+
+// ans no-20...............................................................
+
+// var team1 = ["rahim","karim"];
+// var team2 = ["tanim","biplob","tareq"];
+// var team3 = ["sajjad","arefin"];
+
+// var total = team1+team2+team3;
+// console.log(total);
+
+
+// ans no-21...............................................................
+
+// var fruits = ["banana","orange","apple","mango"]
+// fruits.shift()
+// console.log(fruits);
+
+// ans no-22...............................................................
+
+// var fruits = ["banana","orange","apple","mango"];
+// fruits.slice(3,0,"lichi","pinapple","watermelon");
+// console.log(fruits);
+
+// ans no-23...............................................................
+
+// var x = 10;
+// var y = 5;
+
+// if(x>y){
+//     console.log("x is bigger then y");
+// }
+// else(y>x){
+//     console.log("y is bigger then x");
+// }
+
+// ans no-25...............................................................
+
+// var pen = 10;
+// var price = 185;
+// var quantity = 3;
+
+// var single_pen = price/pen;
+// var ans = single_pen*quantity;
+// console.log(ans);
+
+// ans no-26...............................................................
+
+// var input = 20;
+// var per = 25;
+// var ans = input*(per/100);
+// console.log(ans);
+
+// ans no-27...............................................................
+
+// var fruits = ["banana","orange","apple","mango"]
+// var ans = fruits.indexOf("kiwi");
+// console.log(ans);
+
+
+// ans no-27...............................................................
+
+// var sub1 = 75;
+// var sub2 = 98;
+// var sub3 = 87;
+// var sub4 = 90;
+// var number_of_sub = 4;
+
+// var total_num = sub1+sub2+sub3+sub4;
+// var avg = total_num/number_of_sub;
+// console.log(avg);
+
+// if(avg >=90){
+//     console.log("s grade")
+// }
+// else if(avg >=80 && avg <=90){
+//     console.log("a grade")
+// }
+// else if(avg >=70 && avg <=80){
+//     console.log("b grade")
+// }
+// else if(avg >=60 && avg <=70){
+//     console.log("c grade")
+// }
+// else if(avg >=50 && avg <=60){
+//     console.log("d grade")
+// }
+// else if(avg >=40 && avg <=50){
+//     console.log("e grade")
+// }
+// else if(avg <=40){
+//     console.log("f grade")
+// }
+
+
+// ans no-30...............................................................
+
+// assalamualaikum ami md mehedi hasan rabby. ami akjon student. ami mone kori amader somoy k onek valo vabe baboihar kora uchit. 
+// somoy moto protiti kaj korle porobortite kaj er chap kome jai tai student hisebe ami mone kori amader sokol pora sona o skills er kaj daily akta rules 
+// er vitore niea asa and daily ta follow kora. ete amra kaj e pisiea porbo na ete somoy er kaj somoyer modde ses hoyea jabe. ta sarau amader binodon er
+//  bisoytau mathai rakha lagbe , amader jonno Time er best use gulo khuje ber korte hobe. and amader ta follow korte hobe..
